ff6b3e3b02b5808124a1a472513ca64387d23e3c
DanyHunter <dannytenaglias@gmail.com>
2014-09-03 20:20:51 -0500

    setup spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,21 @@
+require 'brewry'
+require 'VCR'
+require 'webmock'
+
+# with VCR, there is no need to set up an api key to run tests
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/fixtures'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+  c.ignore_localhost = true
+  # remove to start creating cassettes
+  c.allow_http_connections_when_no_cassette = true
+end
+
+RSpec.configure do |c|
+  c.around(:each, :vcr) do |example|
+    name = example.metadata[:full_description].split(/\s+/, 2).join('/').gsub(/[^\w\/]+/, '_')
+    VCR.use_cassette(name) { example.call }
+  end
+end
